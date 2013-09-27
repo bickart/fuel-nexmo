7fb0b5ad11eef98c75c55e9f9df145342a223b9e
Jeff Bickart <jeff@neposystems.com>
2013-09-27 16:03:25 -0400

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,3 @@
-<?php
-
 /**
  * A package to use Nexmo api https://www.nexmo.com.
  *
@@ -7,11 +5,16 @@
  * @version    0.1
  * @author     Jeff Bickart
  * @license    MIT License
- * @copyright  2013 Matthew McConnell
+ * @copyright  2013 Jeff Bickart
  * @link       https://twitter.com/bickart
  */
 Autoloader::add_core_namespace('Nexmo');
 
-Autoloader::add_classes(array(
-    'Nexmo\\NexmoMessage'						=> __DIR__ . '/classes/NexmoMessage.php',
-));
+Autoloader::add_classes(
+    array(
+         'Nexmo\\Nexmo' => __DIR__ . '/classes/nexmo.php',
+         'Nexmo\\Nexmo_Message' => __DIR__ . '/classes/nexmo/nexmomessage.php',
+         'Nexmo\\Nexmo_Account' => __DIR__ . '/classes/nexmo/NexmoAccount.php',
+         'Nexmo\\Nexmo_Receipt' => __DIR__ . '/classes/nexmo/NexmoReceipt.php',
+    )
+);
